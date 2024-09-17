d5ac53d48aac74488bcf60fb52d024316432e438
aktsbot <box.ashishk@gmail.com>
2024-09-17 10:17:05 +0530

    add base routes

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,19 @@ import { Router } from "express";
 
 const router = Router();
 
+// home page where the user can create a poof message
 router.get("/", (req, res) => {
   return res.send("home page");
+});
+
+// after creating a poof, they see this page
+router.get("/done/:id", (req, res) => {
+  return res.send("done page");
+});
+
+// the actual page where the user can see the poof
+router.get("/p/:id", (req, res) => {
+  return res.send("view one page");
 });
 
 export default router;
