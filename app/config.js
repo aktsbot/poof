57abdcee67c57b87ee533aeed401745468878092
aktsbot <box.ashishk@gmail.com>
2024-09-17 10:12:45 +0530

    add helmet headers

--- app/config.js
+++ app/config.js
@@ -4,6 +4,7 @@ const config = {
   port: env.POOF_PORT || 3027,
   appName: env.POOF_APP_NAME || "Poof",
   basePath: env.POOF_BASE_PATH || "/poof", // expecting poof to be hosted in sub-path of a domain like https://app.com/poof
+  env: env.NODE_ENV || "production",
 };
 
 export default config;
--- app/index.js
+++ app/index.js
@@ -2,13 +2,50 @@ import express from "express";
 import morgan from "morgan";
 
 import config from "./config.js";
+import router from "./routes.js";
 
 const app = express();
 
+// --- start of helmet security
+// ref: https://helmetjs.github.io/faq/you-might-not-need-helmet/
+app.disable("x-powered-by");
+// helmet headers
+const HELMET_HEADERS = {
+  "Content-Security-Policy":
+    "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+  "Cross-Origin-Opener-Policy": "same-origin",
+  "Cross-Origin-Resource-Policy": "same-origin",
+  "Origin-Agent-Cluster": "?1",
+  "Referrer-Policy": "no-referrer",
+  "Strict-Transport-Security": "max-age=15552000; includeSubDomains",
+  "X-Content-Type-Options": "nosniff",
+  "X-DNS-Prefetch-Control": "off",
+  "X-Download-Options": "noopen",
+  "X-Frame-Options": "SAMEORIGIN",
+  "X-Permitted-Cross-Domain-Policies": "none",
+  "X-XSS-Protection": "0",
+};
+
+app.use((req, res, next) => {
+  res.set(HELMET_HEADERS);
+  next();
+});
+// --- end of helmet security
+
 app.use(morgan("tiny"));
 
-app.get("/", (req, res) => {
-  return res.json({});
+app.use(`${config.basePath}`, router);
+
+// 404 and other error handlers
+app.use((req, res) => {
+  return res.status(404).send("Not found");
+});
+
+app.use((err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || "Unkown error happened";
+
+  return res.status(statusCode).send(message);
 });
 
 app.listen(config.port, () => {
